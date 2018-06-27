92c1ae25362993618ec842e044fa83a4809eadec
vladimirwaveaccess <vladimir.agafonov2011@gmail.com>
2018-06-27 18:12:45 +0300

    "Wait" method is deleted from "BasePage" class

--- src/test/java/com/envion/Pages/BasePage.java
+++ src/test/java/com/envion/Pages/BasePage.java
@@ -8,9 +8,6 @@ import java.util.concurrent.TimeUnit;
 
 public class BasePage {
     protected static WebDriver driver = DriverFactory.getDriver();
-    protected static void Wait30Seconds() {
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
 
     public BasePage() {
         PageFactory.initElements(driver, this);
